82c794855edb198d3df7f06155b54c5a15fd3907
Pavan Kalyan <pavankalyanyadav321@gmail.com>
2022-04-17 16:55:49 +0530

    Multiple Rides invoice generator

--- CabInvoiceGenerator/InvoiceGenerator.cs
+++ CabInvoiceGenerator/InvoiceGenerator.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
 namespace CabInvoiceGenerator
 {
     public class InvoiceGenerator
@@ -25,17 +24,14 @@ namespace CabInvoiceGenerator
             }
             //Initializing varaibles for Premium Ride
             if (this.type.Equals(RideType.PREMIUM_RIDE))
-
             {
                 this.MINIMUM_COST_PER_KM = 15;
                 this.COST_PER_TIME = 1;
                 this.MINIMUM_FARE = 20;
-
             }
         }
         //Method to Calculate the fare and handling exception
         public double CalculateFare(double distance, int time)
-
         {
             double totalFare = 0.0;
             try
@@ -44,7 +40,6 @@ namespace CabInvoiceGenerator
                 {
                     totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
                 }
-
                 else if (distance <= 0)
                 {
                     throw new CustomException(CustomException.ExceptionType.INVALID_DISTANCE, "Distance should be positive integer");
@@ -53,13 +48,33 @@ namespace CabInvoiceGenerator
                 {
                     throw new CustomException(CustomException.ExceptionType.INVALID_DISTANCE, "Time should be positive integer");
                 }
-
             }
             catch (CustomException ex)
             {
                 Console.WriteLine(ex.message);
             }
             return Math.Max(totalFare, MINIMUM_FARE);
+        }
+        //Method to calculate the totalfare for multiple rides 
+        public InvoiceSummary CalculateFare(Ride[] rides)
+        {
+            double totalFare = 0;
+            try
+            {
+                if (rides == null)
+                {
+                    throw new CustomException(CustomException.ExceptionType.NULL_RIDES, "No rides found");
+                }
+                foreach (Ride ride in rides)
+                {
+                    totalFare += this.CalculateFare(ride.distance, ride.time);
+                }
+            }
+            catch (CustomException ex)
+            {
+                Console.WriteLine(ex.message);
+            }
+            return new InvoiceSummary(rides.Length, totalFare);
         }
     }
 }
--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CabInvoiceGenerator;
-
 namespace TestProject1
 {
     [TestClass]
@@ -11,17 +10,14 @@ namespace TestProject1
         /// </summary>
         [TestMethod]
         [TestCategory("CalculatingFare")]
-
         public void Return_TotalFare_ForNormalRide()
         {
             InvoiceGenerator invoice = new InvoiceGenerator(RideType.NORMAL_RIDE);
             double distance = 5.0;
             int time = 10;
-
             double fare = invoice.CalculateFare(distance, time);
             double expected = 60.0;
             Assert.AreEqual(expected, fare);
-
         }
         /// <summary>
         /// UC5-Return the total fare for premium ride
@@ -30,15 +26,12 @@ namespace TestProject1
         [TestCategory("CalculatingFare")]
         public void Return_TotalFare_ForPremiumRide()
         {
-
             InvoiceGenerator invoice = new InvoiceGenerator(RideType.PREMIUM_RIDE);
             double distance = 15.0;
             int time = 12;
-
             double fare = invoice.CalculateFare(distance, time);
             double expected = 237.0;
             Assert.AreEqual(expected, fare);
-
         }
         /// <summary>
         /// UC1&UC5-Handling the custom exception if distance is negative number or zero.
@@ -54,7 +47,6 @@ namespace TestProject1
                 double distance = -15;
                 int time = 12;
                 double fare = invoice.CalculateFare(distance, time);
-
             }
             catch (CustomException ex)
             {
@@ -75,12 +67,50 @@ namespace TestProject1
                 double distance = 10;
                 int time = 0;
                 double fare = invoice.CalculateFare(distance, time);
-
             }
             catch (CustomException ex)
             {
                 Assert.AreEqual(expected, ex.message);
             }
+        }
+        /// <summary>
+        /// UC2-Returns the totlfare for multiple rides
+        /// </summary>
+        [TestMethod]
+        public void Return_Multiple_Rides_TotalFare()
+        {
+            InvoiceGenerator invoice = new InvoiceGenerator(RideType.PREMIUM_RIDE);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+
+            InvoiceSummary summary = new InvoiceSummary(2, 55.0);
+            InvoiceSummary expected = invoice.CalculateFare(rides);
+            Assert.AreEqual(summary.totalFare, expected.totalFare);
+        }
+        /// <summary>
+        /// UC3-Returns the average ride
+        /// </summary>
+        [TestMethod]
+        public void Return_Multiple_Rides_AverageFare()
+        {
+            InvoiceGenerator invoice = new InvoiceGenerator(RideType.PREMIUM_RIDE);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+
+            InvoiceSummary summary = new InvoiceSummary(2, 55.0);
+            InvoiceSummary expected = invoice.CalculateFare(rides);
+            Assert.AreEqual(summary.avgFare, expected.avgFare);
+        }
+        /// <summary>
+        /// UC3-returns the number of rides
+        /// </summary>
+        [TestMethod]
+        public void Return_Multiple_Rides_NumofRides()
+        {
+            InvoiceGenerator invoice = new InvoiceGenerator(RideType.PREMIUM_RIDE);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+
+            InvoiceSummary summary = new InvoiceSummary(2, 55.0);
+            InvoiceSummary expected = invoice.CalculateFare(rides);
+            Assert.AreEqual(summary.numOfRides, expected.numOfRides);
         }
     }
 }
